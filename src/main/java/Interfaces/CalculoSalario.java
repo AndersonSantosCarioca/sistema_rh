002d6e0afb3c987ef18307d7490d4eff2c02e69f
Anderson Santos <caiocacapoeira3497@gmail.com>
2023-09-06 12:13:33 -0300

    começando criação do switch case

--- src/main/java/Interfaces/CalculoSalario.java
+++ src/main/java/Interfaces/CalculoSalario.java
@@ -1,19 +1,51 @@
 package Interfaces;
 import java.util.Scanner;
-import enums.CalculoSalario;
+
+import javax.sound.midi.Soundbank;
+
 
 public interface CalculoSalario {
 
-    public Class cadastrarFuncionario{
+    public class CalcularSalarioFuncionario{
         Scanner sc = new Scanner(System.in);
-        private String nomeFuncionario {
+        private String cargoFuncionario;
+
+        public void cargoFuncionario(){
+
+            System.out.println("Funcionario. ");
+                String cargoFuncionario = sc.nextLine();
+        }
+                   
+
+        public CalcularSalarioFuncionario(String cargoFuncionario) {
+            this.cargoFuncionario = cargoFuncionario;
+
+            int escolha;
+
+            System.out.println("Digite o cargo do funcionário: ");
+            System.out.println("(1)Atendente\n,(2)Supervisor\n,(3)Gerente");
+
+            switch (escolha) {
+                case 1:
+                    System.out.println();
+                    break;
+                case 2:
+                    System.out.println();
+                case 3:
+                    System.out.println();
             
+                default:
+                    System.out.println("Funcionário não cadastrado.");
+                    break;
+            }
+    
         }
 
-        void nomeFuncionario(){
+       
 
-        }
+        
 
+       
     }
     
 }
